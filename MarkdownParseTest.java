e480f880cbb9f3ad6fd020ab46a1adaec0476203
Yanuk-K <kimyw102699@gmail.com>
2022-04-24 20:14:29 -0700

    test newmd, test-file

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,37 @@
 import static org.junit.Assert.*; 
 import org.junit.*; 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest { 
     @Test
     public void addition() { 
         assertEquals(2, 1 + 1); 
+    }
+    @Test
+    public void getLinkstestfile() throws IOException {
+        ArrayList<String> testArrayList = new ArrayList<>();
+
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+
+        testArrayList.add("https://something.com");
+        testArrayList.add("some-page.html");
+
+        assertEquals(MarkdownParse.getLinks(content), testArrayList);
     } 
+    @Test
+    public void getLinksnewmd() throws IOException {
+        ArrayList<String> testArrayList = new ArrayList<>();
+
+        Path fileName = Path.of("newmd.md");
+        String content = Files.readString(fileName);
+
+        testArrayList.add("https://something.com");
+        testArrayList.add("");
+
+        assertEquals(MarkdownParse.getLinks(content), testArrayList);
+    }
 }
